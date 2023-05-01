8c3d12585ebe4790f45d8f4f7b75f34de2348926
saci zakaria <sacidevelppeur@gmail.com>
2023-05-01 19:33:26 +0200

    Revert "5. custom splash screen in flutter"
    This reverts commit c7f265ba5722bb571e13b7788b3cfb775685452b.

--- lib/splashScreen/my_splash_screen.dart
+++ lib/splashScreen/my_splash_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 
 class MySplashScreen extends StatefulWidget {
   const MySplashScreen({Key? key}) : super(key: key);
@@ -11,21 +10,6 @@ class MySplashScreen extends StatefulWidget {
 class _MySplashScreenState extends State<MySplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Colors.pinkAccent,
-              Colors.purpleAccent,
-            ],
-            begin: FractionalOffset(0.0, 0.0),
-            end: FractionalOffset(1.0, 0.0),
-            stops: [0.0, 1.0],
-            tileMode: TileMode.clamp,
-          ),
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
