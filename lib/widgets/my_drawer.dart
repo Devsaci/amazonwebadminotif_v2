1d4c47a9ae895a172ea8d20080b9f92dae10574a
saci zakaria <sacidevelppeur@gmail.com>
2023-05-01 18:24:57 +0200

    NetworkImage

--- lib/widgets/my_drawer.dart
+++ lib/widgets/my_drawer.dart
@@ -17,13 +17,17 @@ class _MyDrawerState extends State<MyDrawer> {
         children: [
           //header
           Container(
+            padding: const EdgeInsets.only(top: 26, bottom: 12),
             child: Column(
               children: [
                 //user profile image
                 Container(
                   height: 130,
                   width: 130,
-                  child: CircleAvatar(),
+                  child: const CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://images.unsplash.com/photo-1446707052533-0e1d48e08aa9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dHJlc3N8ZW58MHx8MHx8&w=1000&q=80"),
+                  ),
                 ),
                 //user name
                 const Text(
