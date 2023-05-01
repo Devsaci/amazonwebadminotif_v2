f065505e5881dcca6debf5898f2397d01bda75a2
saci zakaria <sacidevelppeur@gmail.com>
2023-05-01 16:51:58 +0200

    primarySwatch: Colors.purple,

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'amazonwebadminotif',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.purple,
       ),
       home: const HomeScreen(),
     );
