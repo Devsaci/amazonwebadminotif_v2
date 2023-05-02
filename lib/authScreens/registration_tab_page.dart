19635f4b9df6eed4c38886af070514b451cecd98
saci zakaria <sacideveloppeur@gmail.com>
2023-05-02 15:33:04 +0200

    class RegistrationTabPage

---
+++ lib/authScreens/registration_tab_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class RegistrationTabPage extends StatefulWidget {
+  const RegistrationTabPage({Key? key}) : super(key: key);
+
+  @override
+  State<RegistrationTabPage> createState() => _RegistrationTabPageState();
+}
+
+class _RegistrationTabPageState extends State<RegistrationTabPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
