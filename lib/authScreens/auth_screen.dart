6c7c5b7364a29daf9c626837e5a3b8083a218f9a
saci zakaria <sacideveloppeur@gmail.com>
2023-05-02 11:38:52 +0200

    DefaultTabController

--- lib/authScreens/auth_screen.dart
+++ lib/authScreens/auth_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class AuthScreen extends StatelessWidget {
   const AuthScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const DefaultTabController(
+      length: 2,
+      child: Scaffold(),
+    );
   }
 }
