663bce659679a119c1554df79a98d41a9a564407
saci zakaria <sacideveloppeur@gmail.com>
2023-05-02 15:32:12 +0200

    class LoginTabPage

---
+++ lib/authScreens/login_tab_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class LoginTabPage extends StatefulWidget {
+  const LoginTabPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginTabPage> createState() => _LoginTabPageState();
+}
+
+class _LoginTabPageState extends State<LoginTabPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
