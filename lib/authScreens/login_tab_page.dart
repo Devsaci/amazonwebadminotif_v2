786780e0bcc8daf3eacae7c89aee48ee57f5fe07
saci zakaria <sacideveloppeur@gmail.com>
2023-05-04 16:20:28 +0200

    CustomTextField emailTextEditingController

--- lib/authScreens/login_tab_page.dart
+++ lib/authScreens/login_tab_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../widgets/custom_text_field.dart';
 
 class LoginTabPage extends StatefulWidget {
   const LoginTabPage({Key? key}) : super(key: key);
-
-
 
   @override
   State<LoginTabPage> createState() => _LoginTabPageState();
@@ -11,6 +12,8 @@ class LoginTabPage extends StatefulWidget {
 
 class _LoginTabPageState extends State<LoginTabPage> {
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  TextEditingController emailTextEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -24,8 +27,19 @@ class _LoginTabPageState extends State<LoginTabPage> {
         ),
         Form(
           key: formKey,
-          child: Column(),
-        )
+          child: Column(
+            children: [
+              //email
+              CustomTextField(
+                textEditingController: emailTextEditingController,
+                iconData: Icons.email,
+                hintText: "Email",
+                isObsecre: false,
+                enabled: true,
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
