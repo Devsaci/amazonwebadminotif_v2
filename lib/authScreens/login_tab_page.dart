f0e3d164468bdecb84f6c304b6886c4075e768af
saci zakaria <sacideveloppeur@gmail.com>
2023-05-04 16:01:26 +0200

    9. design login page ui

--- lib/authScreens/login_tab_page.dart
+++ lib/authScreens/login_tab_page.dart
@@ -10,6 +10,6 @@ class LoginTabPage extends StatefulWidget {
 class _LoginTabPageState extends State<LoginTabPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container();
   }
 }
